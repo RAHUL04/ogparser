12ef19c72ddbcad5a86e1f915cd6da47a44f3297
rahul04 <rahulbaskar007@gmail.com>
2016-05-28 00:19:42 +0530

    exceptions handled

--- app1.py
+++ app1.py
@@ -12,17 +12,25 @@ def index():
 
 @app.route('/results', methods= ['POST','GET']) 
 def results():   
-	
-	want1=request.form['keyword']
+	try:
+		want1=request.form['keyword']
 		
-	header = "http://"
-	url = header+want1
-	print url
-	f = urlopen(url).read()
- 	tree = etree.HTML( f )
-	general = tree.xpath( "//meta[@name='description']" )[0].get("content")	
-	meta = opengraph.OpenGraph(url)
-	print meta
-	return render_template("results.html",data=meta,gog=general)
+		header = "http://"
+		url = header+want1
+		print url
+		f = urlopen(url).read()
+ 		tree = etree.HTML( f )
+		general = tree.xpath( "//meta[@name='description']" )[0].get("content")	
+		meta = opengraph.OpenGraph(url)
+		print meta
+		return render_template("results.html",data=meta,gog=general)
+	except IOError:
+		meta={}
+		general="INvalid Url"
+		return render_template("results.html",data=meta,gog=general)
+	except Exception:
+		meta={}
+		general="these sites dont have a meta tags description they are THE BOSS!!!!!"
+		return render_template("results.html",data=meta,gog=general)
 if __name__=='__main__':
 	app.run(debug=True)
